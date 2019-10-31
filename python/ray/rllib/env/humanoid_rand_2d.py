237221aae7cf6417df362960809347df65928332
Michael Luo <mluo@c67.Millennium.Berkeley.EDU>
2019-10-31 03:51:35 -0700

    more

--- python/ray/rllib/env/humanoid_rand_2d.py
+++ python/ray/rllib/env/humanoid_rand_2d.py
@@ -123,7 +123,7 @@ class HumanoidRandDirec2DEnv(gym.utils.EzPickle, MujocoEnv):
         quad_impact_cost = min(quad_impact_cost, 10)
         reward = lin_vel_cost - quad_ctrl_cost - quad_impact_cost + alive_bonus
         qpos = self.sim.data.qpos
-        done = bool((qpos[2] < 1.0) or (qpos[2] > 2.0))
+        done = False #bool((qpos[2] < 1.0) or (qpos[2] > 2.0))
         return self._get_obs(), reward, done, dict(reward_linvel=lin_vel_cost, reward_quadctrl=-quad_ctrl_cost, reward_alive=alive_bonus, reward_impact=-quad_impact_cost)
 
     def reset_model(self):
